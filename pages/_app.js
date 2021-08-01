78a6841beb5bacac2ff9ec50b6f59b4ab87bfacd
Jan Richter <falnyr@gmail.com>
2021-08-01 14:36:23 +1200

    feat: amplify

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import '../styles/globals.css'
 
+import Amplify from 'aws-amplify';
+import config from '../src/aws-exports';
+Amplify.configure({
+  ...config, ssr: true
+});
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
